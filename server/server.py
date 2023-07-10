d6ea25537c6edec5e51a673b149769f60c049ba5
Hemanth Kumar G <hemanthjois@gmail.com>
2023-07-10 12:06:48 +0530

    Update server.py
    Signed-off-by: Hemanth Kumar G <hemanthjois@gmail.com>

--- server/server.py
+++ server/server.py
@@ -10,7 +10,7 @@ class Greeter(helloworld_pb2_grpc.GreeterServicer):
         return helloworld_pb2.HelloReply(message='Hello, %s!' % request.name)
 
 def serve():
-    port = os.getenv("SERVER_PORT", "50051")
+    port = os.getenv("SERVER_PORT", "32000")
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     helloworld_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
     server.add_insecure_port(f'[::]:{port}')
