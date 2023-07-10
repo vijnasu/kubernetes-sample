0b788bc7fc46857d32339ac19a27dd25a5801715
Hemanth Kumar G <hemanthjois@gmail.com>
2023-07-10 11:27:15 +0530

    Update client.py
    Signed-off-by: Hemanth Kumar G <hemanthjois@gmail.com>

--- client/client.py
+++ client/client.py
@@ -1,15 +1,15 @@
+import os
 import grpc
 import helloworld_pb2
 import helloworld_pb2_grpc
 
-
 def run():
-    with grpc.insecure_channel('grpc-server:50051') as channel:
+    server = os.getenv('GRPC_SERVER_SERVICE_HOST', 'grpc-server')
+    port = os.getenv('GRPC_SERVER_SERVICE_PORT', '32000')
+    with grpc.insecure_channel(f'{server}:{port}') as channel:
         stub = helloworld_pb2_grpc.GreeterStub(channel)
         response = stub.SayHello(helloworld_pb2.HelloRequest(name='you'))
     print("Greeter client received: " + response.message)
 
-
 if __name__ == '__main__':
     run()
-
