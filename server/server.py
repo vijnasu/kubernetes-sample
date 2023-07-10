1cc0dc451fd78df43a1307d0d88070f21b6e1239
Hemanth Kumar G <hemanthjois@gmail.com>
2023-07-10 12:05:32 +0530

    Update server.py
    Signed-off-by: Hemanth Kumar G <hemanthjois@gmail.com>

--- server/server.py
+++ server/server.py
@@ -1,23 +1,21 @@
+import os
 from concurrent import futures
 import grpc
 import helloworld_pb2
 import helloworld_pb2_grpc
-
 
 class Greeter(helloworld_pb2_grpc.GreeterServicer):
 
     def SayHello(self, request, context):
         return helloworld_pb2.HelloReply(message='Hello, %s!' % request.name)
 
-
 def serve():
+    port = os.getenv("SERVER_PORT", "50051")
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     helloworld_pb2_grpc.add_GreeterServicer_to_server(Greeter(), server)
-    server.add_insecure_port('[::]:50051')
+    server.add_insecure_port(f'[::]:{port}')
     server.start()
     server.wait_for_termination()
 
-
 if __name__ == '__main__':
     serve()
-
